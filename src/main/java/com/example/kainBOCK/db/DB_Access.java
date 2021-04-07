852a71565a2adc43f845831c6e27b6f400dbb1ba
LocoNico <71444545+LocoNico@users.noreply.github.com>
2021-04-07 11:41:43 +0200

    tesT

--- src/main/java/com/example/kainBOCK/db/DB_Access.java
+++ src/main/java/com/example/kainBOCK/db/DB_Access.java
@@ -1,2 +1,26 @@
-package com.example.kainBOCK.db;public class DB_Access {
+package com.example.kainBOCK.db;
+
+import java.sql.SQLException;
+
+public class DB_Access {
+
+    private static DB_Access theInstance = null;
+    private DB_Database db;
+
+    public static DB_Access getInstance() throws SQLException {
+        if (theInstance == null) {
+            theInstance = new DB_Access();
+        }
+        return theInstance;
+    }
+
+    private DB_Access() throws SQLException {
+        try {
+            db = new DB_Database();
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("Database problem - possible cause: DB-Driver not found");
+        } catch (SQLException ex) {
+            throw new RuntimeException("Database problem - possible cause: " + ex.toString());
+        }
+    }
 }
--- src/main/java/com/example/kainBOCK/db/DB_CachedConnection.java
+++ src/main/java/com/example/kainBOCK/db/DB_CachedConnection.java
@@ -1,2 +1,48 @@
-package com.example.kainBOCK.db;public class DB_CachedConnection {
+package com.example.kainBOCK.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DB_CachedConnection {
+    private Queue<Statement> statementQueue = new LinkedList<>();                   // Queue: a, b, c
+    private Connection connection;
+
+    public DB_CachedConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * returns a valid statement to the database
+     *
+     * @return
+     * @throws SQLException
+     */
+    public Statement getStatement() throws SQLException {
+        if (connection == null) {
+            throw new RuntimeException("not connected to DB");
+        }
+        if (!statementQueue.isEmpty()) {
+            return statementQueue.poll();
+        }
+        return connection.createStatement();
+    }
+
+    /**
+     * returns a statement to the queue
+     *
+     * @param statement
+     */
+    public void releaseStatement(Statement statement) {
+        if (connection == null) {
+            throw new RuntimeException("not connected to DB");
+        }
+        statementQueue.offer(statement);
+    }
+
+    private Exception RuntimeException(String not_connected_to_DB) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
