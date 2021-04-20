6ed022e4ca7e1394ba4d6e22daff43c3ea438d72
LocoNico <71444545+LocoNico@users.noreply.github.com>
2021-04-20 10:28:38 +0200

    test

--- src/main/java/com/example/kainBOCK/controller/Controller.java
+++ src/main/java/com/example/kainBOCK/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.kainBOCK.controller;
 
+import com.example.kainBOCK.bl.JWT;
 import com.example.kainBOCK.db.DB_Access;
 
 import javax.servlet.*;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @WebServlet(name = "Controller", value = "/Controller")
 public class Controller extends HttpServlet {
@@ -20,5 +22,11 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getParameter("login") != null) {
+            request.getRequestDispatcher("Login").forward(request, response);
+        }
+        else if(request.getParameter("confirmLogin") != null){
+            
+        }
     }
 }
