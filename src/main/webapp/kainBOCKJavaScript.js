435dfa77e1ffdf9ee268f884627e7e855673f200
zmuchd17 <71444462+zmuchd17@users.noreply.github.com>
2021-05-05 10:59:33 +0200

    javascript

--- src/main/webapp/kainBOCKJavaScript.js
+++ src/main/webapp/kainBOCKJavaScript.js
@@ -5,9 +5,3 @@ function openFormRegister() {
     document.getElementById("registerForm").style.display = "block";
 }
 
-function closeFormLogin() {
-    document.getElementById("loginForm").style.display = "none";
-}
-function closeFormRegister() {
-    document.getElementById("registerForm").style.display = "none";
-}
