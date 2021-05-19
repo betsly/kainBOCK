e6a88c5f6df23652543256d0cfc0717d92977e4e
LocoNico <71444545+LocoNico@users.noreply.github.com>
2021-05-19 12:09:07 +0200

    bmi

--- src/main/java/com/example/kainBOCK/bl/JWT.java
+++ src/main/java/com/example/kainBOCK/bl/JWT.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class JWT {
     private static String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
 
-    public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
+    public static String createJWT(int id, String issuer, String subject, long ttlMillis) {
 
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
@@ -23,7 +23,7 @@ public class JWT {
         byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
-        JwtBuilder builder = Jwts.builder().setId(id)
+        JwtBuilder builder = Jwts.builder().setId(id+"")
                 .setIssuedAt(now)
                 .setSubject(subject)
                 .setIssuer(issuer)
